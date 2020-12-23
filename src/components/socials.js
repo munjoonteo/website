db151f6ad6b183e01ceb1d5c65656c5520dbd416
munjoonteo <teomunjoon101@gmail.com>
2020-12-23 15:55:00 +1100

    added socials buttons

---
+++ src/components/socials.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFileAlt } from "@fortawesome/free-solid-svg-icons"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+
+export default function Socials() {
+  let sharingURl = "https://1drv.ms/b/s!AgAjGo-4bxIrxXnrZ-W3Z3kjVsD2?e=corKHM"
+  let base64value = Buffer.from(sharingURl).toString("base64")
+  let resumeLink =
+    "https://api.onedrive.com/v1.0/shares/u!" + base64value + "/root/content"
+  return (
+    <div>
+      <a className="social_button" href={resumeLink}>
+        <FontAwesomeIcon icon={faFileAlt} /> Resume
+      </a>
+      <a className="social_button" href="https://github.com/munjoonteo">
+        <FontAwesomeIcon icon={faGithub} /> Github
+      </a>
+      <a
+        className="social_button"
+        href="https://www.linkedin.com/in/mun-joon-teo-850586195/"
+      >
+        <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+      </a>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Bio from "../components/bio"
+import Socials from "../components/socials"
 
 export default function Home() {
   return (
@@ -7,6 +9,7 @@ export default function Home() {
       <div>Mun Joon Teo</div>
       <div>Software engineering student and musician</div>
       <Bio />
+      <Socials />
     </div>
   )
 }
