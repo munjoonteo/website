bd94d21d9db17571e268c42bab9e3420c862e3cb
munjoonteo <teomunjoon101@gmail.com>
2020-12-23 15:30:18 +1100

    added headers and bio text

---
+++ src/components/bio.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Bio() {
+  return (
+    <div>
+      <div>
+        Hello! I am a second-year studying software engineering at the
+        University of New South Wales. I have a passion for web development and
+        classical music.
+      </div>
+      <div>
+        In my spare time, I play the piano and the bassoon as the Principal
+        Bassoonist of the Sydney Youth Philharmonic Orchestra.
+      </div>
+    </div>
+  )
+}
+
+export default Bio
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Bio from "../components/bio"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <div>Mun Joon Teo</div>
+      <div>Software engineering student and musician</div>
+      <Bio />
+    </div>
+  )
 }
