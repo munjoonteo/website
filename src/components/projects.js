5b5932b14fc9ba3a33b8ee251dfbc94e2aab9080
munjoonteo <teomunjoon101@gmail.com>
2020-12-23 17:04:39 +1100

    added projects component

---
+++ src/components/projects.js
@@ -0,0 +1,11 @@
+import React from "react"
+import styles from "../styles/projects.module.css"
+
+export default function Projects() {
+  return (
+    <div>
+      <div className={styles.heading}>My Projects </div>
+      <div>hi</div>
+    </div>
+  )
+}
