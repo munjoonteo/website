6e35e9350f34ab9598849a1cadf0ee6f52e2584f
munjoonteo <teomunjoon101@gmail.com>
2020-12-23 17:16:31 +1100

    added 404 page

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,7 +4,7 @@ import styles from "../styles/projects.module.css"
 export default function Projects() {
   return (
     <div>
-      <div className={styles.heading}>My Projects </div>
+      <div className={styles.heading}>My Projects</div>
       <div>hi</div>
     </div>
   )
---
+++ src/pages/404.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import styles from "../styles/404.module.css"
+
+export default function NoPage() {
+  return (
+    <Layout>
+      <div className={styles.heading}>Page Not Found</div>
+      <div className={styles.body}>
+        Oops! The page you are looking for does not exist.
+      </div>
+      <Link className={styles.home} to="/">
+        Return to the home page
+      </Link>
+    </Layout>
+  )
+}
