928376d1ebfe53931f4ebbb5c0c403d6f8ece47a
munjoonteo <teomunjoon101@gmail.com>
2020-12-24 14:37:16 +1100

    fixed spacing

--- src/components/bio.js
+++ src/components/bio.js
@@ -4,10 +4,11 @@ import styles from "../styles/bio.module.css"
 export default function Bio() {
   return (
     <div className={styles.bio}>
-      Hello! I am a second-year studying software engineering at the University
-      of New South Wales. I have a passion for web development and classical
-      music.{"\n\n"}In my spare time, I play the piano and the bassoon as the
-      Principal Bassoonist of the Sydney Youth Philharmonic Orchestra.
+      {"\n"}Hello! I am a second-year studying software engineering at the
+      University of New South Wales. I have a passion for web development and
+      classical music.{"\n\n"}In my spare time, I play the piano and the bassoon
+      as the Principal Bassoonist of the Sydney Youth Philharmonic Orchestra.
+      {"\n\n"}
     </div>
   )
 }
