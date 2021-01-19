7825ec359b7b388c4f03d0528ab912b221df118d
munjoonteo <teomunjoon101@gmail.com>
2021-01-19 23:31:43 +1100

    changed to say 3rd year

--- src/components/bio.js
+++ src/components/bio.js
@@ -4,8 +4,8 @@ import styles from "../styles/bio.module.css"
 export default function Bio() {
   return (
     <div className={styles.bio}>
-      {"\n"}Hello! I am a second-year studying software engineering at the
-      University of New South Wales. I have a passion for web development and
+      {"\n"}Hello! I am a penultimate (3rd) year Software Engineering student at the
+      University of New South Wales, Sydney. I have a passion for web development and
       classical music.{"\n\n"}In my spare time, I play the piano and the bassoon
       as the Principal Bassoonist of the Sydney Youth Philharmonic Orchestra.
       {"\n\n"}
