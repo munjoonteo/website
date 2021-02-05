0eaa2890ae8d00a27922da480b34176eefc4acbf
munjoonteo <teomunjoon101@gmail.com>
2021-02-05 18:51:25 +1100

    added japanese project and fixed buttons and spacing

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,16 +3,9 @@ import styles from "../styles/projects.module.css"
 
 const projectData = [
   {
-    title: "Calculator",
-    desc:
-      "A simple calculator written in vanilla Javascript. Keyboard input with both the numpad and number row keys is supported.",
-    link: "https://github.com/munjoonteo/calculator/",
-    try: "https://munjoonteo.github.io/calculator/",
-  },
-  {
     title: "Interactive Chess Game",
     desc:
-      "A web app for two players to play chess with each other. Developed using the p5 Javascript library. In the future, I will also add an AI for single-player games.",
+      "A web app for two players to play chess with each other. Developed using vanilla Javascript and the p5 Javascript library. In the future, I will also add an AI for single-player games.",
     link: "https://github.com/munjoonteo/chess_v2",
     try: "https://munjoonteo.github.io/chess_v2/",
   },
@@ -21,7 +14,21 @@ const projectData = [
     desc:
       "A web app to play the card game We’re Not Really Strangers on your browser. Written using vanilla React.",
     link: "https://github.com/munjoonteo/wnrs",
-    try: "",
+    try: "https://munjoonteo.github.io/wnrs/",
+  },
+  {
+    title: "Japanese Vocabulary List Builder",
+    desc:
+      "A web app to look up Japanese words and add them to a word list, which can then be downloaded as a text file. The text file can be imported into Anki, a Spaced Repetition System (SRS). Powered by the Jisho API and written in vanilla React.",
+    link: "https://github.com/munjoonteo/vocab_list",
+    try: "https://munjoonteo.github.io/vocab_list/",
+  },
+  {
+    title: "Calculator",
+    desc:
+      "A simple calculator written in vanilla Javascript. Keyboard input with both the numpad and number row keys is supported.",
+    link: "https://github.com/munjoonteo/calculator/",
+    try: "https://munjoonteo.github.io/calculator/",
   },
   {
     title: "Mahjong Points Counter",
