bebeb8b4195d6997b7a5db5bca7d462ad5f70536
munjoonteo <teomunjoon101@gmail.com>
2021-02-05 18:51:09 +1100

    updated bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -4,10 +4,10 @@ import styles from "../styles/bio.module.css"
 export default function Bio() {
   return (
     <div className={styles.bio}>
-      {"\n"}Hello! I am a penultimate (3rd) year Software Engineering student at the
-      University of New South Wales, Sydney. I have a passion for web development and
-      classical music.{"\n\n"}In my spare time, I play the piano and the bassoon
-      as the Principal Bassoonist of the Sydney Youth Philharmonic Orchestra.
+      {"\n"}Hello! I am a penultimate (3rd) year Software Engineering student at
+      the University of New South Wales, Sydney. I have a passion for web
+      development and classical music.{"\n\n"}In my spare time, I play the
+      bassoon for the Sydney Youth Orchestra and occasionally play the piano.
       {"\n\n"}
     </div>
   )
